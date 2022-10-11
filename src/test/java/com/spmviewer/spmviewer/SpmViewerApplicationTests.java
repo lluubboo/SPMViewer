1c77f45773096d8e01e0fe0475c0395f4c108cab
lluubboo <114932728+lluubboo@users.noreply.github.com>
2022-10-11 20:00:27 +0200

    Add files via upload
    vygenerovany novy projekt

---
+++ src/test/java/com/spmviewer/spmviewer/SpmViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spmviewer.spmviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
